71167f2a6f09489641bec25f80765935787fcb6e
Mahdi Rashidi <m8rashidi@gmail.com>
2020-10-09 02:43:15 +0330

    add config

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const appOptions = { cors: true };
+  const app = await NestFactory.create(AppModule, appOptions);
+  app.setGlobalPrefix('api');
+
   await app.listen(3000);
 }
 bootstrap();
