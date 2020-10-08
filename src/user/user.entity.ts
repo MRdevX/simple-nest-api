8c2061e6dc14f7c601f1f4b25e3b82cd798458ee
Mahdi Rashidi <m8rashidi@gmail.com>
2020-10-09 03:27:11 +0330

    add user entity

---
+++ src/user/user.entity.ts
@@ -0,0 +1,24 @@
+import { Entity, PrimaryGeneratedColumn, Column, BeforeInsert } from 'typeorm'
+import { IsEmail } from 'class-validator'
+import * as argon2 from 'argon2'
+
+@Entity('user')
+export class UserEntity {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    username: string
+
+    @Column()
+    @IsEmail()
+    email: string
+
+    @Column()
+    password: string
+
+    @BeforeInsert()
+    async hashPassword() {
+        this.password = await argon2.hash(this.password)
+    }
+}
