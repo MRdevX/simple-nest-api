04a3d99bd004e14c02bba7677f563a9ad6b7a6f3
Mahdi Rashidi <m8rashidi@gmail.com>
2020-10-09 02:50:03 +0330

    add book category

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookController } from './book/book.controller';
 import { UserController } from './user/user.controller';
+import { BookCategoryController } from './book-category/book-category.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, BookController, UserController],
+  controllers: [AppController, BookController, UserController, BookCategoryController],
   providers: [AppService],
 })
 export class AppModule {}
