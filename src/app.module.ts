874cca79202fa4a5544a9bebafed5bf68f85f895
Mahdi Rashidi <m8rashidi@gmail.com>
2020-10-09 11:40:56 +0330

    add book category

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { Connection } from 'typeorm'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { BookController } from './book/book.controller'
-import { UserController } from './user/user.controller'
-import { BookCategoryController } from './book-category/book-category.controller'
 import { BookModule } from './book/book.module'
 import { BookCategoryModule } from './book-category/book-category.module'
 import { UserModule } from './user/user.module'
 
 @Module({
     imports: [TypeOrmModule.forRoot(), BookModule, BookCategoryModule, UserModule],
-    controllers: [AppController, BookController, UserController, BookCategoryController],
+    controllers: [AppController],
     providers: [AppService],
 })
 export class AppModule {
