74536abec25ae7945e1b3a9447bc8c82c5c81c6e
Mahdi Rashidi <m8rashidi@gmail.com>
2020-10-10 18:26:15 +0330

    update typeorm config

--- src/helpers/dto/user.dto.ts
+++ src/helpers/dto/user.dto.ts
@@ -1,21 +1,27 @@
 import { IsNotEmpty } from 'class-validator'
+import { ApiProperty } from '@nestjs/swagger'
 
 export class CreateUserDto {
     @IsNotEmpty()
+    @ApiProperty()
     readonly username: string
 
     @IsNotEmpty()
+    @ApiProperty()
     readonly email: string
 
     @IsNotEmpty()
+    @ApiProperty()
     readonly password: string
 }
 
 export class LoginUserDto {
     @IsNotEmpty()
+    @ApiProperty()
     readonly email: string
 
     @IsNotEmpty()
+    @ApiProperty()
     readonly password: string
 }
 
